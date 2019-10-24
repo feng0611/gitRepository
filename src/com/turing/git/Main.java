58ea9edc60326648bd4d8bbeec729b48ec35ed46
admin <2020144858@qq.com>
2019-10-24 15:39:19 +0800

    idea的项目

---
+++ src/com/turing/git/Main.java
@@ -0,0 +1,12 @@
+package com.turing.git;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello git");
+    }
+
+}
